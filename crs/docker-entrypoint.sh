ddc1c5c78fd3b8d65daf9ba76dd014ddb2bfb6b6
root <alexum2013@yandex.ru>
2021-03-15 12:06:24 +0300

    Доработка совместимости с 8.3.18

--- crs/docker-entrypoint.sh
+++ crs/docker-entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 if [ "$1" = "crserver" ]; then
-  exec gosu usr1cv8 /opt/1C/v8.3/i386/crserver -port 1542 -d /home/usr1cv8/.1cv8
+  if [ -d "/opt/1cv8/x86_64/${ONEC_VERSION}/" ]; then
+    exec gosu usr1cv8 /opt/1cv8/i386/${ONEC_VERSION}/crserver -port 1542 -d /home/usr1cv8/.1cv8
+  else
+    exec gosu usr1cv8 /opt/1C/v8.3/i386/crserver -port 1542 -d /home/usr1cv8/.1cv8
+  fi
 fi
 
 exec "$@"
