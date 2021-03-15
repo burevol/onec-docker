7313f728bd4f599f302f6157d71533bf53ac7209
root <alexum2013@yandex.ru>
2021-03-15 12:17:03 +0300

    Совместимость с 8.3.18

--- crs-apache/docker-entrypoint.sh
+++ crs-apache/docker-entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # start crserver
-exec gosu usr1cv8 /opt/1C/v8.3/i386/crserver -port 1542 -d /home/usr1cv8/.1cv8/repo -daemon &
+if [ -d "/opt/1cv8/x86_64/${ONEC_VERSION}/" ]; then
+  exec gosu usr1cv8 /opt/1cv8/i386/${ONEC_VERSION}/crserver -port 1542 -d /home/usr1cv8/.1cv8/repo -daemon &
+else
+  exec gosu usr1cv8 /opt/1C/v8.3/i386/crserver -port 1542 -d /home/usr1cv8/.1cv8/repo -daemon &
+fi
 
 # start apache2
 source /etc/apache2/envvars
