490b055b38b63384416e3dbbfbc035f19b9d6d81
root <alexum2013@yandex.ru>
2021-03-15 12:27:38 +0300

    Исправление совместимости

--- crs-apache/docker-entrypoint.sh
+++ crs-apache/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # start crserver
-if [ -d "/opt/1cv8/x86_64/${ONEC_VERSION}/" ]; then
+if [ -d "/opt/1cv8/i386/${ONEC_VERSION}/" ]; then
   exec gosu usr1cv8 /opt/1cv8/i386/${ONEC_VERSION}/crserver -port 1542 -d /home/usr1cv8/.1cv8/repo -daemon &
 else
   exec gosu usr1cv8 /opt/1C/v8.3/i386/crserver -port 1542 -d /home/usr1cv8/.1cv8/repo -daemon &
