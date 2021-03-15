d51a3baa322edfc5e7a89f6df9aa4c16e866ec7c
root <alexum2013@yandex.ru>
2021-03-15 12:42:26 +0300

    Исправление опечатки

--- crs-apache/docker-entrypoint.sh
+++ crs-apache/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 # start crserver
 if [ -d "/opt/1cv8/i386/$ONEC_VERSION/" ]; then
-  exec gosu usr1cv8 /opt/1cv8/i386/$ONEC_VERSION/crserver" -port 1542 -d /home/usr1cv8/.1cv8/repo -daemon &
+  exec gosu usr1cv8 /opt/1cv8/i386/$ONEC_VERSION/crserver -port 1542 -d /home/usr1cv8/.1cv8/repo -daemon &
 else
   exec gosu usr1cv8 /opt/1C/v8.3/i386/crserver -port 1542 -d /home/usr1cv8/.1cv8/repo -daemon &
 fi
