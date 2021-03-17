2aea13b9c2181c09b7b8d45c167a87a7ae538451
root <alexum2013@yandex.ru>
2021-03-17 14:16:09 +0300

    Сервер х64 доведен до рабочего состояния для версий >= 8.3.18

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+eval $(cat /opt/1cv8/version)
+
 if [ "$1" = "ragent" ]; then
-  exec gosu usr1cv8 /opt/1C/v8.3/x86_64/ragent
+  if ! [ -d /opt/1cv8/x86_64/$ONEC_VERSION ]; then
+    exec gosu usr1cv8 /opt/1C/v8.3/x86_64/ragent
+  else
+    exec gosu usr1cv8 /opt/1cv8/x86_64/$ONEC_VERSION/ragent 
+  fi
 fi
 
 exec "$@"
