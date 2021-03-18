bd57f6a21f86881d6de6fd21131b0a73ad28e7be
root <alexum2013@yandex.ru>
2021-03-18 15:52:20 +0300

    Пробуем запустить отладку

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -6,7 +6,7 @@ if [ "$1" = "ragent" ]; then
   if ! [ -d /opt/1cv8/x86_64/$ONEC_VERSION ]; then
     exec gosu usr1cv8 /opt/1C/v8.3/x86_64/ragent
   else
-    exec gosu usr1cv8 /opt/1cv8/x86_64/$ONEC_VERSION/ragent 
+    exec gosu usr1cv8 /opt/1cv8/x86_64/$ONEC_VERSION/ragent /debug -http
   fi
 fi
 
